d45f52a8cdbe199fac761688fd67d6876188f8f8
joaopierre <jppierre90@gmail.com>
2025-02-13 09:09:59 -0300

    Add TrackAPI data source and resolvers for fetching tracks and authors

---
+++ server/src/resolvers.js
@@ -0,0 +1,12 @@
+const resolvers = {
+    Query: {
+        // returns an array of track objects that are displayed on the
+        // homepage grid
+
+        tracksForHome: () => {
+
+        }
+    }
+}
+
+module.exports = resolvers;
